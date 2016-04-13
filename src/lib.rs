e7b5655c389cea2a37e565b4c43b2715ee9ebc6e
Pierre Chevalier <pierrechevalier83@hotmail.fr>
2016-04-13 02:10:30 +0100

    Upgrade to latest glutin

--- src/lib.rs
+++ src/lib.rs
@@ -141,7 +141,7 @@ impl GlutinWindow {
             },
             Some(E::KeyboardInput(glutin::ElementState::Released, _, Some(key))) =>
                 Some(Input::Release(Button::Keyboard(map_key(key)))),
-            Some(E::MouseMoved((x, y))) => {
+            Some(E::MouseMoved(x, y)) => {
                 let f = self.window.hidpi_factor();
                 let x = x as f64 / f as f64;
                 let y = y as f64 / f as f64;
@@ -155,9 +155,9 @@ impl GlutinWindow {
                 }
                 Some(Input::Move(Motion::MouseCursor(x, y)))
             }
-            Some(E::MouseWheel(MouseScrollDelta::PixelDelta(x, y))) =>
+            Some(E::MouseWheel(MouseScrollDelta::PixelDelta(x, y), _)) =>
                 Some(Input::Move(Motion::MouseScroll(x as f64, y as f64))),
-            Some(E::MouseWheel(MouseScrollDelta::LineDelta(x, y))) =>
+            Some(E::MouseWheel(MouseScrollDelta::LineDelta(x, y), _)) =>
                 Some(Input::Move(Motion::MouseScroll(x as f64, y as f64))),
             Some(E::MouseInput(glutin::ElementState::Pressed, button)) =>
                 Some(Input::Press(Button::Mouse(map_mouse(button)))),
